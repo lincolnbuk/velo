6c657236dd6c725aebab214e4105f73033cdfa3f
Adriano Lincoln <sit-bull@hotmail.com>
2026-01-24 17:14:41 -0300

    Challenges order-result-id and order-result-status

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -3,21 +3,27 @@ import { test, expect } from '@playwright/test'
 /// AAA - Arrange, Act, Assert
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
-  // Arrange
+  // (Arrange)
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
   
-  // Act 
-  await page.getByTestId('search-order-id').fill('VLO-2WHJQO')
+  // (Act)
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-2WHJQO')
   await page.getByTestId('search-order-button').click()
 
-  // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
+  //await page.locator('//label[text()="Número do Pedido"]/..//input').fill('VLO-2WHJQO')
+  //await page.getByLabel('Número do Pedido').fill('VLO-2WHJQO')
+  //await page.getByPlaceholder('Ex: VLO-ABC123').fill('VLO-2WHJQO') 
+
+  // (Assert)
+  await expect(page.getByTestId('order-result-VLO-2WHJQO')).toContainText('VLO-2WHJQO'); //Desafio 'order-result-id'
+  //await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 10_000}) 
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-2WHJQO')
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
+  await expect(page.getByTestId('order-result-VLO-2WHJQO')).toContainText('APROVADO'); //Desafio 'order-result-status'
+  //await expect(page.getByTestId('order-result-status')).toBeVisible()
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
-
 })
